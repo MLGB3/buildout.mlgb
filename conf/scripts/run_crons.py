a22d3b34858ec10748485487ac5d734164763430
Calvin Butcher <calvin.butcher@bodleian.ox.ac.uk>
2014-05-07 16:29:55 +0100

    testing run_crons.py

--- conf/scripts/run_crons.py
+++ conf/scripts/run_crons.py
@@ -1,6 +1,6 @@
 import subprocess
 
-subprocess.call('/home/xiaofeng/sites/xiaofeng/bin/python /home/xiaofeng/index/authortitle_to_xml.py')
+subprocess.call('/home/xiaofeng/index/authortitle_to_xml.py')
 
 def main():
     pass
