f957e96f0fe8c05d8e7f10bacee1d06421bd2523
Calvin Butcher <calvin.butcher@bodleian.ox.ac.uk>
2014-05-07 11:13:16 +0100

    added scripts files for executing crons

---
+++ conf/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+version = open(os.path.join("version.txt")).read().strip()
+
+setup(
+    name='mysite',
+    version=version,
+    packages=[''],
+    include_package_data=True,
+    license='BSD License',
+    description='A django product',
+    long_description='Should have a readme file',
+    url='http://www.example.com/',
+    author='Your Name',
+    author_email='yourname@example.com',
+    classifiers=[
+        'Framework :: Django',
+    ],
+    entry_points="""
+        [console_scripts]
+        run_crons = mysite.mlgb.scripts.run_crons:main
+    """,
+
+)
