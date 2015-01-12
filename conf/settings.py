138fb40deac93f0f3183dfcac4a26ff0b8d14a17
Michael Davis <michael.davis@bodleian.ox.ac.uk>
2015-01-12 15:08:20 +0000

    Set the username and password for mysql in development.cfg

--- conf/settings.py
+++ conf/settings.py
@@ -9,12 +9,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'      # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'mlgb'         # Or path to database file if using sqlite3.
-DATABASE_USER = 'mlgbAdmin'    # Not used with sqlite3.
-DATABASE_PASSWORD = 'blessing' # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'mysql'
+DATABASE_NAME = 'mlgb'
+DATABASE_USER = '${users:mysql_user}'
+DATABASE_PASSWORD = '${passwords:mysql_user}'
+DATABASE_HOST = ''  # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''  # Set to empty string for default. Not used with sqlite3.
 
 TIME_ZONE = 'Europe/London'
 
